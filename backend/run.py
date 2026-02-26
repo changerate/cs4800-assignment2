501f80df032072e18db5cc1bbe3f29a149005230
Carlos <carlosfvargas526@gmail.com>
2026-02-26 00:35:55 -0800

    for the server

--- backend/run.py
+++ backend/run.py
@@ -7,9 +7,17 @@ from app import create_app
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
 
+# if __name__ == "__main__":
+#     app.run(
+#         host=os.getenv("HOST", "127.0.0.1"),
+#         port=int(os.getenv("PORT", "5000")),
+#         debug=os.getenv("FLASK_DEBUG", "0") == "1",
+#     )
+
+# For the server: 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("HOST", "127.0.0.1"),
+        host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", "5000")),
         debug=os.getenv("FLASK_DEBUG", "0") == "1",
     )
